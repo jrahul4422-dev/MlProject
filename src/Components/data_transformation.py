2e114362fc6fa6664ef223058a6b6b06eec7217e
jrahul4422-dev <jrahul4422@gmail.com>
2026-04-27 11:38:04 -0400

    fix data transformation pipeline

--- src/Components/data_transformation.py
+++ src/Components/data_transformation.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from dataclasses import dataclass
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 
 import numpy as np
 import pandas as pd
@@ -17,7 +18,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path: str = os.path.join("artifacts", "preprocessor.pkl")
+    preprocessor_obj_file_path = os.path.join(BASE_DIR, "artifacts", "preprocessor.pkl")
 
 
 class DataTransformation:
@@ -81,10 +82,10 @@ class DataTransformation:
 
             target_column_name = "math_score"
 
-            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            input_feature_train_df = train_df.drop(columns=[target_column_name])
             target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            input_feature_test_df = test_df.drop(columns=[target_column_name])
             target_feature_test_df = test_df[target_column_name]
 
             logging.info("Applying preprocessing object on train and test data")
